680b15ea4cea63ce435c1056cbe15140bf4d5766
sky往事 <403991842@qq.com>
2018-05-17 18:25:46 +0800

    Add first project

---
+++ app/src/main/java/android/util/keyUtils.java
@@ -0,0 +1,80 @@
+package android.util;
+
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CaptureRequest;
+import android.hardware.camera2.CaptureResult;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
+/**
+ * Created by xp018347 on 4/17/18.
+ */
+
+public class keyUtils {
+    public static CaptureRequest.Key<?> keyConstructor(String name, Class<?> type) {
+        try {
+            Class clazz = Class.forName("android.hardware.camera2.CaptureRequest$Key");
+            Class[] params = {String.class, Class.class};
+            Constructor constructor = clazz.getConstructor(params);
+            return (CaptureRequest.Key<?>) constructor.newInstance(name, type);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (RuntimeException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static CaptureResult.Key<?> keyCaptureResultConstructor(String name, Class<?> type) {
+        try {
+            Class clazz = Class.forName("android.hardware.camera2.CameraCharacteristics$Key");
+            Class[] params = {String.class, Class.class};
+            Constructor constructor = clazz.getConstructor(params);
+            return (CaptureResult.Key<?>) constructor.newInstance(name, type);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (RuntimeException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static CameraCharacteristics.Key<?> keyCharacteristicsConstructor(String name, Class<?> type) {
+        try {
+            Class clazz = Class.forName("android.hardware.camera2.CameraCharacteristics$Key");
+            Class[] params = {String.class, Class.class};
+            Constructor constructor = clazz.getConstructor(params);
+            return (CameraCharacteristics.Key<?>) constructor.newInstance(name, type);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (RuntimeException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
